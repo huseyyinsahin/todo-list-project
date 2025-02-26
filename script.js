ec40b23a54a970a00ed0a7139b5d6a3d336fca07
huseyyinsahin <huseysahin19@outlook.com>
2025-02-26 23:37:28 +0300

    revised

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const input = document.querySelector("#input");
-const add = document.querySelector("#add");
-const list = document.querySelector("#list");
 const select = document.querySelector("#select");
+const btnAdd = document.querySelector("#add");
+const list = document.querySelector("#list");
 
 const Toast = Swal.mixin({
   toast: true,
@@ -15,134 +15,85 @@ const Toast = Swal.mixin({
   },
 });
 
-input.focus();
+const addList = (taskList) => {
+  const newLi = document.createElement("li");
+  newLi.id = taskList.id;
 
-function saveLocalStorage() {
+  const newP = document.createElement("p");
+  newP.textContent = taskList.task;
+
+  const importance = document.createElement("span");
+  importance.textContent = taskList.taskSelect;
+  importance.style.whiteSpace = "nowrap";
+  importance.style.margin = "0px 10px";
+
+  const edit = document.createElement("a");
+  edit.classList.add("fa-solid", "fa-pen-to-square");
+  edit.style.cursor = "pointer";
+  edit.style.marginRight = "10px";
+
+  const del = document.createElement("i");
+  del.classList.add("fa-solid", "fa-trash");
+  del.style.cursor = "pointer";
+  del.style.marginRight = "10px";
+
+  newLi.appendChild(newP);
+  newLi.appendChild(importance);
+  newLi.appendChild(edit);
+  newLi.appendChild(del);
+  list.appendChild(newLi);
+
+  taskList.lineThrough && newP.classList.add("line-through");
+};
+
+const setLocalStorage = (data) => {
+  const storageTasks = JSON.parse(localStorage.getItem("todoList")) || [];
+  storageTasks.push(data);
+  localStorage.setItem("todoList", JSON.stringify(storageTasks));
+};
+
+const getLocalStorage = () => {
+  const data = JSON.parse(localStorage.getItem("todoList")) || [];
+  data.forEach((taskList) => {
+    addList(taskList);
+  });
+};
+
+document.addEventListener("DOMContentLoaded", getLocalStorage);
+
+btnAdd.addEventListener("click", () => {
   const data = {
-    list: list.innerHTML,
+    id: Date.now(),
+    task: input.value,
+    taskSelect: select.value,
+    lineThrough: false,
   };
-  localStorage.setItem("listData", JSON.stringify(data));
-}
 
-function loadLocalStorage() {
-  const listD = localStorage.getItem("listData");
-  if (listD) {
-    const data = JSON.parse(listD);
-    list.innerHTML = data.list;
-  }
-}
-
-document.addEventListener("DOMContentLoaded", loadLocalStorage);
-
-function newTask() {
   if (input.value == "") {
     Toast.fire({
       icon: "error",
       title: "Adding task failed",
     });
   } else {
-    const newLi = document.createElement("li");
-    const newP = document.createElement("p");
-    newP.textContent = input.value;
-
-    const importance = document.createElement("span");
-    importance.textContent = select.value;
-    importance.style.whiteSpace = "nowrap";
-    importance.style.margin = "0px 10px";
-
-    const edit = document.createElement("a");
-    edit.classList.add("fa-solid", "fa-pen-to-square");
-    edit.style.cursor = "pointer";
-    edit.style.marginRight = "10px";
-
-    const del = document.createElement("i");
-    del.classList.add("fa-solid", "fa-trash");
-    del.style.cursor = "pointer";
-    del.style.marginRight = "10px";
-
-    newLi.appendChild(newP);
-    newLi.appendChild(importance);
-    newLi.appendChild(edit);
-    newLi.appendChild(del);
-    list.appendChild(newLi);
-    input.value = "";
-    input.focus();
-    saveLocalStorage();
-
-    Toast.fire({
-      icon: "success",
-      title: "New task added successfully",
-    });
+    addList(data);
   }
-}
-
-add.addEventListener("click", () => {
-  newTask();
-});
-
-document.body.addEventListener("keydown", (e) => {
-  if (e.key == "Enter") {
-    newTask();
-  }
+  setLocalStorage(data);
+  input.value = "";
 });
 
 list.addEventListener("click", (e) => {
-  if (e.target.tagName === "I") {
-    const selectedLi = e.target.parentElement;
-    selectedLi.remove();
-    saveLocalStorage();
+  const data = JSON.parse(localStorage.getItem("todoList")) || [];
 
-    Toast.fire({
-      icon: "warning",
-      title: "Task deleted",
-    });
-  }
-});
-
-document.body.addEventListener("keydown", (e) => {
-  if (e.key == "Delete") {
-    list.firstElementChild.remove();
-    saveLocalStorage();
-
-    Toast.fire({
-      icon: "warning",
-      title: "Task deleted",
-    });
-  }
-});
-
-list.addEventListener("click", (e) => {
-  if (e.target.tagName === "P") {
-    e.target.classList.toggle("line-through");
-    saveLocalStorage();
-  }
-});
-
-list.addEventListener("click", (e) => {
-  if (e.target.tagName === "A") {
-    const selectedP = e.target.previousElementSibling.previousElementSibling;
-    const editInput = document.createElement("input");
-    editInput.style.padding = "5px 10px";
-    editInput.style.fontSize = "1rem";
-    editInput.style.outline = "none";
-    editInput.style.borderRadius = "0.5rem";
-    editInput.style.border = "none";
-    editInput.value = selectedP.textContent;
-
-    selectedP.parentElement.replaceChild(editInput, selectedP);
-
-    editInput.addEventListener("blur", () => {
-      if (editInput.value == "") {
-        Toast.fire({
-          icon: "error",
-          title: "Task edit failed",
-        });
-      } else {
-        selectedP.textContent = editInput.value;
-        editInput.parentElement.replaceChild(selectedP, editInput);
-        saveLocalStorage();
+  if (e.target.tagName == "P") {
+    const id = e.target.parentElement.id;
+    const taskData = data.map((item) => {
+      if (item.id == id) {
+        item.lineThrough = !item.lineThrough;
       }
+      return item;
     });
-    editInput.focus();
+    document.querySelectorAll("li")
+    taskData.forEach((item) => addList(item));
+    localStorage.setItem("todoList", JSON.stringify(taskData));
   }
 });
